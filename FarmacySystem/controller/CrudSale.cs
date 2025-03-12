fc7adbd2955e671f318dae7085f11361fedf7c8f
Holiveira090 <h.oliveira2005@hotmail.com>
2025-03-12 16:33:46 -0400

    feat: crud medicine, sale, supplier

---
+++ FarmacySystem/controller/CrudSale.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FarmacySystem.model;
+using FarmacySystem.data;
+
+namespace FarmacySystem.controller
+{
+    public class CrudSale
+    {
+        public void InsertSales(int idS, string customerS, DateTime saleDateS, decimal totalValueS, int salesmanIdS)
+        {
+            using (var db = new AppDbContext())
+            {
+                db.Sales.Add(new Sale { Id = idS, Customer = customerS, SaleDate = saleDateS, TotalValue = totalValueS, SalesmanId = salesmanIdS });
+                db.SaveChanges();
+            }
+        }
+        public List<string> ListSales()
+        {
+            List<string> SalesList = new List<string>();
+
+            using (var db = new AppDbContext())
+            {
+                var sales = db.Sales.ToList();
+                foreach (var sale in sales)
+                {
+                    SalesList.Add($"Id: {sale.Id} Cliente: {sale.Customer} Data da venda: {sale.SaleDate} valor total: {sale.TotalValue} Usuario: {sale.SalesmanId}");
+                }
+            }
+            return SalesList;
+        }
+        public void SalesUpdate(int id, string customer, DateTime sale_date, decimal total_value, int salesman_id)
+        {
+            using (var db = new AppDbContext())
+            {
+                var sale = db.Sales.Find(id);
+                if (sale != null)
+                {
+                    sale.Id = id;
+                    sale.Customer = customer;
+                    sale.SaleDate = sale_date;
+                    sale.TotalValue = total_value;
+                    sale.SalesmanId = salesman_id;
+                    db.SaveChanges();
+                    System.Console.WriteLine("Venda atualizada com sucesso");
+                }
+                else
+                {
+                    System.Console.WriteLine("Venda não encontrada");
+                }
+            }
+        }
+        public void SalesDelete(int id)
+        {
+            using (var db = new AppDbContext())
+            {
+                var sale = db.Sales.Find(id);
+                if (sale != null)
+                {
+                    db.Sales.Remove(sale);
+                    db.SaveChanges();
+                    System.Console.WriteLine("Venda deletada com sucesso");
+                }
+                else
+                {
+                    System.Console.WriteLine("Venda não encontrada");
+                }
+            }
+        }
+    }
+}
---
+++ FarmacySystem/controller/CrudSupplier.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FarmacySystem.model;
+using FarmacySystem.data;
+
+namespace FarmacySystem.controller
+{
+    public class CrudSupplier
+    {
+        public void InsertSuppliers(int idS, string nameS, string cnpjS, string phoneS, string zip_codeS, int numberS)
+        {
+            using (var db = new AppDbContext())
+            {
+                db.Suppliers.Add(new Supplier { Id = idS, Name = nameS, Cnpj = cnpjS, Phone = phoneS, ZipCode = zip_codeS, Number = numberS });
+                db.SaveChanges();
+            }
+        }
+        public List<string> ListSuppliers()
+        {
+            List<string> SuppliersList = new List<string>();
+
+            using (var db = new AppDbContext())
+            {
+                var suppliers = db.Suppliers.ToList();
+                foreach (var supplier in suppliers)
+                {
+                    SuppliersList.Add($"Id: {supplier.Id} Nome: {supplier.Name} Cnpj: {supplier.Cnpj} Telefone: {supplier.Phone} Cep: {supplier.ZipCode} numero: {supplier.Number}");
+                }
+            }
+            return SuppliersList;
+        }
+        public void SuppliersUpdate(int id, string name, string cnpj, string phone, string zip_code, int number)
+        {
+            using (var db = new AppDbContext())
+            {
+                var supplier = db.Suppliers.Find(id);
+                if (supplier != null)
+                {
+                    supplier.Id = id;
+                    supplier.Name = name;
+                    supplier.Cnpj = cnpj;
+                    supplier.Phone = phone;
+                    supplier.ZipCode = zip_code;
+                    supplier.Number = number;
+                    db.SaveChanges();
+                    System.Console.WriteLine("Fornecedor atualizado com sucesso");
+                }
+                else
+                {
+                    System.Console.WriteLine("Fornecedor não encontrado");
+                }
+            }
+        }
+        public void SupplierDelete(int id)
+        {
+            using (var db = new AppDbContext())
+            {
+                var supplier = db.Sales.Find(id);
+                if (supplier != null)
+                {
+                    db.Sales.Remove(supplier);
+                    db.SaveChanges();
+                    System.Console.WriteLine("Fornecedor deletado com sucesso");
+                }
+                else
+                {
+                    System.Console.WriteLine("supplier não encontrado");
+                }
+            }
+        }
+    }
+}
